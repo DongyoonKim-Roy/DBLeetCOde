252a661ee77cc3e0600ec6dd00fa8bb389ad21d5
DongYoon Kim (Roy) <111625791+DongyoonKim-Roy@users.noreply.github.com>
2023-10-17 14:45:01 -0400

    Time: 13 ms (89.94%), Space: 43.6 MB (75.22%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.java
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -6,19 +6,17 @@ class Solution {
         int freq = 0;
         int curr = 0;
         
-        
         for(int n : nums){
             if(n % 2 == 1)
                 continue;
             curr = numMap.getOrDefault(n, 0) + 1;
             numMap.put(n, curr);
             
-            if(curr > freq || curr == freq && n < res){
+            if(curr > freq || (curr == freq && n < res)){
                 freq = curr;
                 res = n;
             }
         } //for
-        
         
         return res;
     }
