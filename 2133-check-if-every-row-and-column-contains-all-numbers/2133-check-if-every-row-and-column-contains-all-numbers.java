343a2c8ab047ec5fc16f5f8cdf3e3af030cecf3a
DongYoon Kim (Roy) <111625791+DongyoonKim-Roy@users.noreply.github.com>
2023-10-20 09:54:56 -0400

    Time: 3 ms (99.45%), Space: 43.4 MB (99.89%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -2,18 +2,29 @@ class Solution {
     public boolean checkValid(int[][] matrix) {
         //brute force
         int len = matrix.length;
+        boolean[] flag = new boolean[len];
         
         for(int i = 0; i < matrix[0].length; i++){
-            HashSet<Integer> cols = new HashSet<Integer>();
-            HashSet<Integer> rows = new HashSet<Integer>();
+            for(int j = 0; j < matrix[i].length; j++)
+                flag[matrix[i][j] - 1] = true;
             
-            for(int j = 0; j < matrix[i].length; j++){
-                cols.add(matrix[j][i]);
-                rows.add(matrix[i][j]);
+            for(int k = 0; k < len; k++){
+                if(!flag[k])
+                    return false;
+                else
+                    flag[k] = false;
             }
+        }
+        
+        for(int i = 0; i < matrix[0].length; i++){
+            for(int j = 0; j < matrix[i].length; j++)
+                flag[matrix[j][i] - 1] = true;
             
-            if(cols.size() != len || rows.size() != len){
-                return false;
+            for(int k = 0; k < len; k++){
+                if(!flag[k])
+                    return false;
+                else
+                    flag[k] = false;
             }
         }
         return true;
