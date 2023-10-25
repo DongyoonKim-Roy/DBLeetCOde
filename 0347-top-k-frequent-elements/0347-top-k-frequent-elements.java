d4598f352f5527539f4e8fabe2de74c55080313e
DongYoon Kim (Roy) <111625791+DongyoonKim-Roy@users.noreply.github.com>
2023-10-25 10:44:14 -0400

    Time: 54 ms (5.07%), Space: 47.5 MB (66.27%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -3,18 +3,14 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         int[] res = new int[k];
         
-        for(int n : nums){
+        for(int n : nums)
             map.put(n, map.getOrDefault(n, 1) + 1);
-        }
-        
         
         for(int i = 0; i < k; i++){
             int temp = Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
             map.remove(temp);
             res[i] = temp;
-            
         }
-        
         return res;
     }
 }
